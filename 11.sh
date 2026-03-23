93adc54ae827c89a54d6685b25f0549731e3d57c
Qingjiang Li <2022213360@mail.hfut.edu.cn>
2026-03-23 16:10:59 +0800

    change log

---
+++ 11.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+git filter-branch --env-filter '
+OLD_EMAIL=im.qingjiangli@gamil.com
+CORRECT_NAME=Qingjiang Li
+CORRECT_EMAIL=2022213360@mail.hfut.edu.cn
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+export GIT_COMMITTER_NAME="$CORRECT_NAME"
+export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+export GIT_AUTHOR_NAME="$CORRECT_NAME"
+export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi' --tag-name-filter cat -- --branches --tags
